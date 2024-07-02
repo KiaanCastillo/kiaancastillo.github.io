376118dbde3c1c5642875385ff1d2d74c1e63632
Kiaan Castillo <kiaan_castillo@sfu.ca>
2024-07-01 20:44:17 -0700

    Add GitHub actions script for deploying React app on push to main branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kiaancastillo.github.io",
   plugins: [react()],
   resolve: {
     alias: [
