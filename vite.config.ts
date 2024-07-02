f193dad86b0987b3038b03a2aa0633c05b724d17
Kiaan Castillo <kiaan_castillo@sfu.ca>
2024-07-01 21:53:21 -0700

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "url";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kiaancastillo.github.io",
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "@c",
-        replacement: fileURLToPath(
-          new URL("./src/components", import.meta.url)
-        ),
-      },
-      {
-        find: "@a",
-        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
-      },
-    ],
-  },
 });
