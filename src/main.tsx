05c5494d6b49b7d0fe0e2571025e943e38175340
Kiaan Castillo <kiaan_castillo@sfu.ca>
2024-07-06 21:50:39 -0700

    switch to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import { ErrorNotFoundPage, ProjectPage } from "./pages";
 
@@ -8,12 +8,12 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="projects/:name" element={<ProjectPage />} />
         <Route path="*" element={<ErrorNotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
